090dd5af18dec2411c266fba850f3f19bf18584c
shktkt219 <shktkt219@gmail.com>
2023-11-14 11:54:36 +0100

    Passing Children

---
+++ src/components/Alert.tsx
@@ -0,0 +1,12 @@
+// shortcut rafce
+import { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className='alert alert-primary'>{children}</div>;
+};
+
+export default Alert
